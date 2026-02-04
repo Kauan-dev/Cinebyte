dd4846ed07ffea93faf9f61917cee2107c093fb0
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-02-03 22:30:40 -0300

    feat: cria header do projeto

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+import logo from "../../../assets/imdb.png";
+import FavoritesIcon from "../../../assets/icons/star.svg?react";
+
 export function Header() {
   return (
-    <header>
-      <h3>HEADER</h3>
+    <header className="font-oswald flex h-17 items-center justify-between bg-black px-7 py-4">
+      <Link to="/">
+        <img className="h-10" src={logo} alt="" />
+      </Link>
+      <div className="flex gap-8">
+        <Link className="tracking-widest" to="/">
+          MOVIES
+        </Link>
+        <Link className="tracking-widest" to="/">
+          TV SHOWS
+        </Link>
+      </div>
+      <Link to="/">
+        <FavoritesIcon className="size-7.5 hover:text-[#e6b91e]" />
+      </Link>
     </header>
   );
 }
