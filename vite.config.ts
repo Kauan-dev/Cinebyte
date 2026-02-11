f5d10b22f1814b15b310c66045aa59b1f504c70b
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-02-10 23:06:35 -0300

    chore: adiciona shadcn/ui ao projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
