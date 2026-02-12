272cc77fd64eede311ee417f9a4b0ab76777b36b
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-02-12 04:11:41 -0300

    feat: adiciona novas seções de mídia na Home e customiza a scrollbar da aplicação

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,32 +8,58 @@ import { MediaSection } from "../../components/sections/MediaSection";
 import { Container } from "@/components/layout/Container";
 
 export function Home() {
-  const [nowPlaying, setNowPlaying] = useState<Media[]>([]);
-  const [upcoming, setUpcoming] = useState<Media[]>([]);
-  const [onTheAir, setOnTheAir] = useState<Media[]>([]);
-  const [popular, setPopular] = useState<Media[]>([]);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState<Media[]>([]);
+  const [popularSeries, setPopularSeries] = useState<Media[]>([]);
+  const [upcomingMovies, setUpcomingMovies] = useState<Media[]>([]);
+  const [weekTrendingSeries, setWeekTrendingSeries] = useState<Media[]>([]);
+  const [popularMovies, setPopularMovies] = useState<Media[]>([]);
+  const [topRatedSeries, setTopRatedSeries] = useState<Media[]>([]);
+  const [topRatedMovies, setTopRatedMovies] = useState<Media[]>([]);
 
-  const itemLimit = 18;
+  const itemLimit = 16;
 
   useEffect(() => {
     async function loadHomeData() {
       try {
         const [
-          nowPlayingResponse,
-          upcomingResponse,
-          onTheAirResponse,
-          popularResponse,
+          nowPlayingMoviesResponse,
+          popularSeriesResponse,
+          upcomingMoviesResponse,
+          weekTrendingSeriesResponse,
+          popularMoviesResponse,
+          topRatedSeriesResponse,
+          topRatedMoviesResponse,
         ] = await Promise.all([
           api.get("movie/now_playing"),
-          api.get("movie/upcoming"),
-          api.get("tv/on_the_air"),
           api.get("tv/popular"),
+          api.get("movie/upcoming"),
+          api.get("trending/tv/week"),
+          api.get("movie/popular"),
+          api.get("tv/top_rated"),
+          api.get("movie/top_rated"),
         ]);
 
-        setNowPlaying(nowPlayingResponse.data.results.slice(0, itemLimit));
-        setUpcoming(upcomingResponse.data.results.slice(0, itemLimit));
-        setOnTheAir(onTheAirResponse.data.results.slice(0, itemLimit));
-        setPopular(popularResponse.data.results.slice(0, itemLimit));
+        setNowPlayingMovies(
+          nowPlayingMoviesResponse.data.results.slice(0, itemLimit),
+        );
+        setPopularSeries(
+          popularSeriesResponse.data.results.slice(0, itemLimit),
+        );
+        setUpcomingMovies(
+          upcomingMoviesResponse.data.results.slice(0, itemLimit),
+        );
+        setWeekTrendingSeries(
+          weekTrendingSeriesResponse.data.results.slice(0, itemLimit),
+        );
+        setPopularMovies(
+          popularMoviesResponse.data.results.slice(0, itemLimit),
+        );
+        setTopRatedSeries(
+          topRatedSeriesResponse.data.results.slice(0, itemLimit),
+        );
+        setTopRatedMovies(
+          topRatedMoviesResponse.data.results.slice(0, itemLimit),
+        );
       } catch (error) {
         console.error("Erro ao carregar dados da Home", error);
       }
@@ -56,10 +82,13 @@ export function Home() {
       </Container>
 
       <div className="flex flex-col gap-3">
-        <MediaSection title="Filmes em cartaz" data={nowPlaying} />
-        <MediaSection title="Em breve nos cinemas" data={upcoming} />
-        <MediaSection title="Series em exibição" data={onTheAir} />
-        <MediaSection title="Series populares da semana" data={popular} />
+        <MediaSection title="Filmes em cartaz" data={nowPlayingMovies} />
+        <MediaSection title="Series populares" data={popularSeries} />
+        <MediaSection title="Em breve nos cinemas" data={upcomingMovies} />
+        <MediaSection title="Series em alta" data={weekTrendingSeries} />
+        <MediaSection title="Filmes populares" data={popularMovies} />
+        <MediaSection title="Séries mais bem avaliadas" data={topRatedSeries} />
+        <MediaSection title="Filmes mais bem avaliados" data={topRatedMovies} />
       </div>
     </div>
   );
