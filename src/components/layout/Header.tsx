613f51b4dd959e93732e657249a51c65ce1a7973
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-03-10 16:38:57 -0300

    style: adiciona efeito de transparência no header da aplicação

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,7 @@ import { Button } from "../ui/button";
 
 export function Header() {
   return (
-    <Container className="font-google sticky top-0 z-100 mb-4 flex h-17 items-center justify-center bg-black font-semibold">
+    <Container className="font-google sticky top-0 z-100 mb-4 flex h-17 items-center justify-center bg-black/75 font-semibold backdrop-blur-md">
       <header className="flex w-full items-center justify-between">
         <div className="flex items-center gap-10">
           <Link to="/" title="Home">
