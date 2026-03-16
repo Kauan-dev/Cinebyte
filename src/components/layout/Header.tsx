9e2d529f74f48cea22889486790ab7757856428f
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-03-15 22:48:29 -0300

    feat: destaca link ativo no header com NavLink

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,30 +1,37 @@
-import { Link } from "react-router-dom";
 import { Container } from "./Container";
 import logo from "../../assets/images/logo.png";
 import { Bookmark } from "lucide-react";
 import { SearchBar } from "./SearchBar";
 import { Button } from "../ui/button";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
+  const navLinkClass = ({ isActive }: { isActive: boolean }) =>
+    `flex h-full items-center text-[15px] tracking-wide ${
+      isActive
+        ? "text-yellow-400 border-b-2 border-b-amber-400"
+        : "text-gray-400"
+    }`;
+
   return (
     <Container className="font-google sticky top-0 z-100 mb-4 flex h-16 items-center justify-center bg-black/75 font-semibold backdrop-blur-md">
-      <header className="flex w-full items-center justify-between">
-        <div className="flex items-center gap-10">
-          <Link to="/" title="Home">
+      <header className="flex h-full w-full items-center justify-between">
+        <div className="flex h-full items-center gap-10">
+          <NavLink to="/" title="Home" end>
             <img
               className="h-10 min-w-20 duration-300 ease-in-out hover:scale-105"
               src={logo}
               alt=""
             />
-          </Link>
+          </NavLink>
 
-          <div className="hidden gap-8 md:flex">
-            <Link className="text-[15px] tracking-wide" to="/movies">
+          <div className="hidden h-full gap-8 md:flex">
+            <NavLink className={navLinkClass} to="/movies">
               FILMES
-            </Link>
-            <Link className="text-[15px] tracking-wide" to="/tv-series">
+            </NavLink>
+            <NavLink className={navLinkClass} to="/tv-series">
               SÉRIES
-            </Link>
+            </NavLink>
           </div>
         </div>
 
@@ -35,10 +42,11 @@ export function Header() {
             size={"icon-lg"}
             variant={"secondary"}
             className="hidden md:flex"
+            asChild
           >
-            <Link to="/watch-later" title="Watch list">
-              <Bookmark className="size-5.5 duration-300 ease-in-out hover:text-[#e6b91e]" />
-            </Link>
+            <NavLink to="/watch-later" title="Watch list">
+              <Bookmark className="size-6 duration-300 ease-in-out hover:text-[#e6b91e]" />
+            </NavLink>
           </Button>
         </div>
       </header>
