e5ec37533b5ef455a1292b95cc7993d825958e48
Kauan Almeida <kauan.almeida1300@gmail.com>
2026-04-08 23:53:27 -0300

    feat: searchbar sempre visível no desktop e com toggle no mobile

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -1,7 +1,11 @@
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Search } from "lucide-react";
-import { InputGroup, InputGroupInput } from "@/components/ui/input-group";
+import {
+  InputGroup,
+  InputGroupInput,
+  InputGroupAddon,
+} from "@/components/ui/input-group";
 import { Button } from "../ui/button";
 
 export function SearchBar() {
@@ -25,7 +29,9 @@ export function SearchBar() {
     <div className="flex items-center gap-2">
       <form onSubmit={handleSearch}>
         <InputGroup
-          className={`h-10 bg-transparent! transition-all duration-300 ease-out ${isSearchBarActive ? "opacity-100" : "opacity-0"}`}
+          className={`h-10 bg-transparent! transition-all duration-300 ease-out md:opacity-100 ${
+            isSearchBarActive ? "opacity-100" : "opacity-0"
+          }`}
         >
           <InputGroupInput
             ref={inputRef}
@@ -33,11 +39,20 @@ export function SearchBar() {
             placeholder="Buscar..."
             value={search}
             onChange={(e) => setSearch(e.target.value)}
+            className="ml-1 md:ml-0"
           />
+          <InputGroupAddon className="hidden md:block">
+            <Search />
+          </InputGroupAddon>
         </InputGroup>
       </form>
 
-      <Button variant={"outline"} size={"icon-lg"} onClick={searchBarToggle}>
+      <Button
+        variant={"outline"}
+        size={"icon-lg"}
+        onClick={searchBarToggle}
+        className="md:hidden"
+      >
         <Search className="size-6" />
       </Button>
     </div>
